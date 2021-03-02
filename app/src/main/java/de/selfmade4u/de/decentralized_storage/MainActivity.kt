46363bdeca3bebced79b3445733a1330727fb041
Hedtke, Moritz <Moritz.Hedtke@t-online.de>
2021-03-02 23:22:13 +0100

    some experiments

--- app/src/main/java/de/selfmade4u/de/decentralized_storage/MainActivity.kt
+++ app/src/main/java/de/selfmade4u/de/decentralized_storage/MainActivity.kt
@@ -2,12 +2,18 @@ package de.selfmade4u.de.decentralized_storage
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.selfmade4u.de.decentralized_storage.ui.theme.DecentralizedStorageTheme
 
 class MainActivity : AppCompatActivity() {
@@ -29,10 +35,19 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun MessageList(messages: List<String>) {
+    Column(Modifier.fillMaxWidth())  {
+        messages.forEach { message ->
+            Text(text = message, textAlign = TextAlign.Center)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DecentralizedStorageTheme {
-        Greeting("Android")
+        MessageList(messages = listOf("hi", "jo", "yeeh"))
     }
 }
--- app/src/main/java/de/selfmade4u/de/decentralized_storage/provider/DecentralizedContentProvider.kt
+++ app/src/main/java/de/selfmade4u/de/decentralized_storage/provider/DecentralizedContentProvider.kt
@@ -1,13 +1,11 @@
 package de.selfmade4u.de.decentralized_storage.provider
 
-import android.content.ContentProvider
-import android.content.ContentValues
 import android.database.Cursor
 import android.database.MatrixCursor
-import android.net.Uri
 import android.os.CancellationSignal
 import android.os.ParcelFileDescriptor
 import android.provider.DocumentsContract
+import android.provider.DocumentsContract.Root.FLAG_SUPPORTS_RECENTS
 import android.provider.DocumentsProvider
 import de.selfmade4u.de.decentralized_storage.R
 
@@ -53,12 +51,94 @@ class DecentralizedContentProvider : DocumentsProvider() {
         // It's possible to have multiple roots (e.g. for multiple accounts in the
         // same app) -- just add multiple cursor rows.
         result.newRow().apply {
-            add(DocumentsContract.Root.COLUMN_ROOT_ID, "Root")
+                        // FLAG_SUPPORTS_CREATE means at least one directory under the root supports
+            // creating documents. FLAG_SUPPORTS_RECENTS means your application's most
+            // recently used documents will show up in the "Recents" category.
+            // FLAG_SUPPORTS_SEARCH allows users to search all documents the application
+            // shares.
+            add(
+                    DocumentsContract.Document.COLUMN_FLAGS,
+                            DocumentsContract.Document.FLAG_SUPPORTS_DELETE
+            )
+
+            // COLUMN_TITLE is the root title (e.g. Gallery, Drive).
+            add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "Your Public Key");
+
+            // This document id cannot change after it's shared.
+            add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, "de.selfmade4u.decentralized_storage.documents.root.public-key")
+
+            // The child MIME types are used to filter the roots and only present to the
+            // user those roots that contain the desired type somewhere in their file hierarchy.
+            add(DocumentsContract.Document.COLUMN_MIME_TYPE, "image/png")
+            add(DocumentsContract.Document.COLUMN_SIZE, 1_000_000)
+            add(DocumentsContract.Document.COLUMN_LAST_MODIFIED, 0);
+        }
+
+        return result
+    }
+
+    override fun queryDocument(documentId: String?, projection: Array<out String>?): Cursor {
+        // Use a MatrixCursor to build a cursor
+        // with either the requested fields, or the default
+        // projection if "projection" is null.
+        val result = MatrixCursor(DEFAULT_ROOT_PROJECTION)
+
+        // If user is not logged in, return an empty root cursor.  This removes our
+        // provider from the list entirely.
+        //if (!isUserLoggedIn()) {
+        //    return result
+        //}
+
+        // It's possible to have multiple roots (e.g. for multiple accounts in the
+        // same app) -- just add multiple cursor rows.
+        result.newRow().apply {
+            // FLAG_SUPPORTS_CREATE means at least one directory under the root supports
+            // creating documents. FLAG_SUPPORTS_RECENTS means your application's most
+            // recently used documents will show up in the "Recents" category.
+            // FLAG_SUPPORTS_SEARCH allows users to search all documents the application
+            // shares.
+            add(
+                    DocumentsContract.Document.COLUMN_FLAGS,
+                    DocumentsContract.Document.FLAG_SUPPORTS_DELETE
+            )
+
+            // COLUMN_TITLE is the root title (e.g. Gallery, Drive).
+            add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "Your Public Key");
+
+            // This document id cannot change after it's shared.
+            add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, "de.selfmade4u.decentralized_storage.documents.root.public-key")
+
+            // The child MIME types are used to filter the roots and only present to the
+            // user those roots that contain the desired type somewhere in their file hierarchy.
+            add(DocumentsContract.Document.COLUMN_MIME_TYPE, "image/png")
+            add(DocumentsContract.Document.COLUMN_SIZE, 1_000_000)
+            add(DocumentsContract.Document.COLUMN_LAST_MODIFIED, 0);
+        }
+
+        return result
+    }
+
+    override fun queryChildDocuments(parentDocumentId: String?, projection: Array<out String>?, sortOrder: String?): Cursor {
+        // Use a MatrixCursor to build a cursor
+        // with either the requested fields, or the default
+        // projection if "projection" is null.
+        val result = MatrixCursor(DEFAULT_DOCUMENT_PROJECTION)
+
+        // If user is not logged in, return an empty root cursor.  This removes our
+        // provider from the list entirely.
+        //if (!isUserLoggedIn()) {
+        //    return result
+        //}
+
+        // It's possible to have multiple roots (e.g. for multiple accounts in the
+        // same app) -- just add multiple cursor rows.
+        result.newRow().apply {
+            add(DocumentsContract.Root.COLUMN_ROOT_ID, "Profile")
 
             // You can provide an optional summary, which helps distinguish roots
             // with the same title. You can also use this field for displaying an
             // user account name.
-            add(DocumentsContract.Root.COLUMN_SUMMARY, "Roootsflidh")
+            add(DocumentsContract.Root.COLUMN_SUMMARY, "Personal Decentralized Storage")
 
             // FLAG_SUPPORTS_CREATE means at least one directory under the root supports
             // creating documents. FLAG_SUPPORTS_RECENTS means your application's most
@@ -76,24 +156,16 @@ class DecentralizedContentProvider : DocumentsProvider() {
             add(DocumentsContract.Root.COLUMN_TITLE, context?.getString(R.string.app_name))
 
             // This document id cannot change after it's shared.
-            add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, "somerandomdocid")
+            add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, "de.selfmade4u.decentralized_storage.documents.root")
 
             // The child MIME types are used to filter the roots and only present to the
             // user those roots that contain the desired type somewhere in their file hierarchy.
-            add(DocumentsContract.Root.COLUMN_MIME_TYPES, "text/plain")
-            add(DocumentsContract.Root.COLUMN_AVAILABLE_BYTES, 1)
+            add(DocumentsContract.Root.COLUMN_MIME_TYPES, "image/png")
+            add(DocumentsContract.Root.COLUMN_AVAILABLE_BYTES, 1000_000_000)
             add(DocumentsContract.Root.COLUMN_ICON, R.drawable.ic_launcher_foreground)
         }
 
         return result
-    }
-
-    override fun queryDocument(documentId: String?, projection: Array<out String>?): Cursor {
-        TODO("Not yet implemented")
-    }
-
-    override fun queryChildDocuments(parentDocumentId: String?, projection: Array<out String>?, sortOrder: String?): Cursor {
-        TODO("Not yet implemented")
     }
 
     override fun openDocument(documentId: String?, mode: String?, signal: CancellationSignal?): ParcelFileDescriptor {
